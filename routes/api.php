fd471b0e61f9effb14c8b0d00aee69afb57f76be
Kev9794 <k.natanael88@gmail.com>
2022-06-08 22:31:16 +0700

    Praktikum 1 : implementasi fitur logout

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);  
+Route::post('login', [AuthController::class, 'login']);
 
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+});
